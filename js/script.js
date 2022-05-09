85b3687f03ecfec07b765a1d0a2e5a84c468f457
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-09 13:44:06 -0300

    tentando novamente o meu script.js para validar

--- js/script.js
+++ js/script.js
@@ -1,57 +1,52 @@
 class Validator {
-
     constructor() {
         this.validations = [
             'data-min-length',
         ]
     }
+
     validate(form) {
         let inputs = form.getElementsByTagName('input');
         let inputsArray = [...inputs];
 
-         inputsArray.forEach(function(input){
+        inputsArray.forEach(function(input) {
             for(let i = 0; this.validations.length > i; i++) {
                 if(input.getAttribute(this.validations[i]) != null) {
-                    
-                  let method = this.validations[i].replace('data-', '').replace('-', '');
 
-                  let value = input.getAttribute(this.validations[i]);
+                    let method = this.validations[i].replace('data-', '').replace('-', '');
+                    let value = input.getAttribute(this.validations[i]);
 
-                  this[method](input, value);
+                    this[method](input, value);
                 }
             }
         }, this);
     }
-    minlenght(input, minValue) {
-
+    minlength(input, minValue) {
         let inputLength = input.value.length;
-
-        let errorMessage = `O campo precisar ter menos ${minValue} caracteres`;
+        let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
 
         if(inputLength < minValue) {
             this.printMessage(input, errorMessage);
         }
-    } 
-
+    }
     printMessage(input, msg) {
-       let template = document.querySelector('.error-validation').cloneNode(true);
+        let template = document.querySelector('.error-validation').cloneNode(true);
 
-       template.textContent = msg;
+        template.textContent = msg;
 
-       let inputParent = input.parentNode;
+        let inputParent = input.parentNode;
 
-       template.classList.remove('template');
+        template.classList.remove('template');
 
-       inputParent.appendChild(template);
+        inputParent.appendChild(template);
     }
 }
 
-let form = document.getElementById("register-form");
+let form  = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
 let validator = new Validator();
 
 submit.addEventListener('click', function(e) {
     e.preventDefault();
-
     validator.validate(form);
-});  
+});
